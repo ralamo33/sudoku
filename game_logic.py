5ffa12852a2b2b8f5b265f97c16d14b566d245cd
ralamo33 <ralamo33@gmail.com>
2020-01-14 09:48:45 +0000

    Board ready is complete.

--- game_logic.py
+++ game_logic.py
@@ -1,3 +1,5 @@
+import random
+
 """The logic behind running a game of sudoku."""
 
 board = [[-1 * 9] * 9]
@@ -12,3 +14,31 @@ class Tile:
         if not isinstance(num, int):
             raise Exception("Wrong input type.")
         self.num = num
+
+def ready_board(reveal = 10):
+    """Solve a few tiles to ready the board for the player.
+
+    :parameter reveal The number of tiles presolved for the player.
+
+    :return board The board ready for the player
+    """
+    coordinates = list(board_coordinates())
+    for r in range(0, reveal):
+        revealed = random.randint(0, len(coordinates) - 1)
+        selected = coordinates[revealed]
+        del coordinates[revealed]
+        solve_tile(board, selected)
+    return board
+
+def solve_tile(board, selected):
+    pass
+
+
+
+def board_coordinates():
+    for i in range(0, 9):
+        for j in range(0, 9):
+            yield (i, j)
+
+if __name__ == "__main__":
+    ready_board()
